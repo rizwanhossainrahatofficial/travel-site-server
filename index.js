b451ba92ac5c9897d19f51ac4eb483d89277bcfc
rizwanhossainrahat <rizwanhossainrahat400@gmail.com>
2022-08-06 20:42:34 +0600

    a

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iwcqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+console.log(uri)
 
 async function run(){
     try{
